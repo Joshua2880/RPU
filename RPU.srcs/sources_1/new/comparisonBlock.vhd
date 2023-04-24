a56c127b6e3c6228cc8108f2b86247a06f142239
egeozgul <egeozgul@gmail.com>
2023-04-24 04:38:30 -0400

    Update comparisonBlock.vhd
    Tested, works very well.
    Used bcl_width of 4 for testing btw.

--- RPU.srcs/sources_1/new/comparisonBlock.vhd
+++ RPU.srcs/sources_1/new/comparisonBlock.vhd
@@ -5,11 +5,12 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Comparison_Block is
   generic(REGISTER_WIDTH : integer := 64;
-  Op_NONE : STD_LOGIC_VECTOR := "00";
-  Op_NEW : STD_LOGIC_VECTOR := "01";
-  Op_LFT : STD_LOGIC_VECTOR := "10";
-  Op_BLFT :STD_LOGIC_VECTOR :=  "11");
- -- type op_enum is (Op_NEW,Op_LFT,Op_BLFT);
+  CONTROL_LINE_WIDTH : integer := 6;
+  Op_NEW : STD_LOGIC_VECTOR := "00";
+  Op_LFT : STD_LOGIC_VECTOR := "01";
+  Op_BLFT :STD_LOGIC_VECTOR :=  "10";
+ Op_NONE : STD_LOGIC_VECTOR := "11");
+  -- type op_enum is (Op_NEW,Op_LFT,Op_BLFT);
  
   port(
   		clk     : in  STD_LOGIC:='0';
@@ -21,18 +22,18 @@ entity Comparison_Block is
         
         op      : in STD_LOGIC_VECTOR(2-1 downto 0);
         
-        x_cntr      : in  STD_LOGIC_VECTOR(6-1 downto 0) := (others => '0');
-        y_cntr      : in  STD_LOGIC_VECTOR(6-1 downto 0) := (others => '0');
-        z_cntr      : in  STD_LOGIC_VECTOR(6-1 downto 0) := (others => '0');
+        x_cntr      : in  STD_LOGIC_VECTOR(CONTROL_LINE_WIDTH-1 downto 0) := (others => '0');
+        y_cntr      : in  STD_LOGIC_VECTOR(CONTROL_LINE_WIDTH-1 downto 0) := (others => '0');
+        z_cntr      : in  STD_LOGIC_VECTOR(CONTROL_LINE_WIDTH-1 downto 0) := (others => '0');
         
-        A_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        B_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        C_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        D_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        E_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        F_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        G_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0');
-        H_in      : in  STD_LOGIC_VECTOR(64-1 downto 0) := (others => '0')
+        A_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        B_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        C_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        D_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        E_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        F_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        G_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0');
+        H_in      : in  STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0) := (others => '0')
         
         );
 end entity Comparison_Block;
@@ -40,8 +41,8 @@ end entity Comparison_Block;
 architecture A1 of Comparison_Block is
  
 
-signal register_msb  : STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0):= (others => '0');
-signal bcl_width  : STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0):= (others => '0');
+signal register_msb  : STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0):= "1000000000000000000000000000000000000000000000000000000000000000";
+signal bcl_width  : STD_LOGIC_VECTOR(REGISTER_WIDTH-1 downto 0):= "0000000000000000000000000000000000000000000000000000000000000100";
 
 signal z_a: STD_LOGIC := '0';
 signal o_a: STD_LOGIC := '0';
@@ -69,8 +70,23 @@ signal condition : STD_LOGIC_VECTOR(3-1 downto 0):= (others => '0');
 
            
 begin
-n_0 <= (others => '0') when (op = Op_NONE) else A_in;
-d_0 <= (others => '0') when (op = Op_NONE) else E_in;
+n_0 <= (others => '0') when (op = Op_NONE) else
+    A_in when (op = Op_NEW) else 
+    A_in when (op = Op_LFT) and (x_cntr >= 2) else
+    A_in when (op = Op_LFT) and (x_cntr = 1) else
+    A_in when (op = Op_BLFT) and (x_cntr >= 2) and (y_cntr >= 2) else
+    A_in when (op = Op_BLFT) and (x_cntr = 1) and (y_cntr = 1) else
+    (others => '0');
+
+d_0 <= (others => '0') when (op = Op_NONE) else
+    E_in when (op = Op_NEW) else 
+    E_in when (op = Op_LFT) and (x_cntr >= 2) else
+    E_in when (op = Op_LFT) and (x_cntr = 1) else
+    E_in when (op = Op_BLFT) and (x_cntr >= 2) and (y_cntr >= 2) else
+    E_in when (op = Op_BLFT) and (x_cntr = 1) and (y_cntr = 1) else
+    (others => '0');
+
+--d_0 <= (others => '0') when (op = Op_NONE) else E_in;
 
 condition <=  "000" when (op = Op_LFT) and (x_cntr >= 2) else 
               "001" when (op = Op_LFT) and (x_cntr = 1) else
@@ -107,127 +123,71 @@ d_3 <=  E_in + F_in + G_in + H_in when condition = "010" else
                     (others => '0');
                 
                 
---  A2: if op = Op_LFT and x_cntr >= 2 generate
---      n_1 <= A_in + B_in;
---      d_1 <= E_in + F_in;
---  end generate A2;
 
---  A3: if op = Op_LFT and x_cntr = 1 generate
---      n_1 <= B_in;
---      d_1 <= F_in;
---  end generate A3;
-
---  A4: if op = Op_BLFT and x_cntr >= 2 and y_cntr >= 2 generate
---      n_1 <= A_in + B_in;
---      n_2 <= A_in + C_in;
---      n_3 <= A_in + B_in + C_in + D_in;
---      d_1 <= E_in + F_in;
---      d_2 <= E_in + G_in;
---      d_3 <= E_in + F_in + G_in + H_in;
---  end generate A4;
-
---  A5: if op = Op_BLFT and x_cntr = 1 and y_cntr = 1 generate
---      n_1 <= B_in;
---      n_2 <= C_in;
---      n_3 <= D_in;
---      d_1 <= F_in;
---      d_2 <= G_in;
---      d_3 <= H_in;
---  end generate A5;
-
---  A6: if op /= Op_NEW and op /= Op_LFT and op /= Op_BLFT generate
---      n_1 <= (others => '0');
---      n_2 <= (others => '0');
---      n_3 <= (others => '0');
---      d_1 <= (others => '0');
---      d_2 <= (others => '0');
---      d_3 <= (others => '0');
---  end generate A6;
-
-
-
-
-
-z_0 <= '1' when (n_0 = 0) and (z_cntr = 0) else
-		'1' when  ((n_0 and register_msb) = 0) and ((d_0 and register_msb) = 0) and (z_cntr = 0)  else
-		'1' when  (n_0 < 0) and (d_0 < 0)and (z_cntr = 0) else
-	  '1' when (d_0 <= n_0)and (z_cntr = 1) else
-	  '0' when (z_cntr /= 1) and (z_cntr /= 0);
+z_0 <= '1' when (n_0 = 0) and (z_cntr = "00000") else
+		'1' when  ((n_0 and register_msb) = 0) and ((d_0 and register_msb) = 0) and (z_cntr = "00000")  else
+		'1' when  (n_0 < 0) and (d_0 < 0)and (z_cntr = "00000") else
+	  '1' when (d_0 <= n_0)and (z_cntr = "00001") else
+	  '1' when (unsigned(n_0) <= unsigned(shift_left(unsigned(d_0),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0' ;--when (z_cntr /= "00001") and (z_cntr /= "00000");
 
 	  
-z_1 <= '1' when (n_1 = 0) and (z_cntr = 0) else
-		'1' when  ( (n_1 and register_msb) = 0) and ( (d_1 and register_msb) = 0)and (z_cntr = 0)  else
-		'1' when  (n_1 < 0) and (d_1 < 0)and (z_cntr = 0) else
-	  '1' when  (d_1 <= n_1)and (z_cntr = 1) else
-	  '0' when  signed(n_1) <= shift_left(signed(d_1),1);
+z_1 <= '1' when (n_1 = 0) and (z_cntr = "00000") else
+		'1' when  ( (n_1 and register_msb) = 0) and ( (d_1 and register_msb) = 0)and (z_cntr = "00000")  else
+		'1' when  (n_1 < 0) and (d_1 < 0)and (z_cntr = "00000") else
+	  '1' when  (d_1 <= n_1)and (z_cntr = "00001") else
+	  	  '1' when (unsigned(n_1) <= unsigned(shift_left(unsigned(d_1),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0' ;--when  signed(n_1) <= shift_left(signed(d_1),1);
 
-z_2 <= '1' when (n_2 = 0) and (z_cntr = 0) else
-		'1' when  ( (n_2 and register_msb) = 0) and ( (d_2 and register_msb) = 0)and (z_cntr = 0)  else
-		'1' when  (n_2 < 0) and (d_2 < 0)and (z_cntr = 0) else
-	  '1' when  (d_2 <= n_2)and (z_cntr = 1) else
-	  '0' when  signed(n_2) <= shift_left(signed(d_2),1);
-
-
-z_3 <= '1' when (n_3 = 0) and (z_cntr = 0) else
-		'1' when  ( (n_3 and register_msb) = 0) and ( (d_3 and register_msb) = 0)and (z_cntr = 0)  else
-		'1' when  (n_3 < 0) and (d_3 < 0)and (z_cntr = 0) else
-	  '1' when  (d_3 <= n_3)and (z_cntr = 1) else
-	  '0' when  signed(n_3) <= shift_left(signed(d_3),1);
+z_2 <= '1' when (n_2 = 0) and (z_cntr = "00000") else
+		'1' when  ( (n_2 and register_msb) = 0) and ( (d_2 and register_msb) = 0)and (z_cntr = "00000")  else
+		'1' when  (n_2 < 0) and (d_2 < 0)and (z_cntr = "00000") else
+			  '1' when (unsigned(n_2) <= unsigned(shift_left(unsigned(d_2),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '1' when  (d_2 <= n_2)and (z_cntr = "00001") else
+	  '0';-- when  signed(n_2) <= shift_left(signed(d_2),1);
 
 
-o_0 <= '1' when (n_0 = 0)and (z_cntr = 0)  else
-		'1' when  (n_0 < 0) and ( (d_0 and register_msb) = 0)and (z_cntr = 0)  else
-		'1' when  (n_0 > 0) and (d_0 < 0)and (z_cntr = 0) else
-	  '1' when n_0 <= d_0 else
-	  '0' when  shift_left(signed(d_0),1) <= signed(n_0);
+z_3 <= '1' when (n_3 = 0) and (z_cntr = "00000") else
+		'1' when  ( (n_3 and register_msb) = 0) and ( (d_3 and register_msb) = 0)and (z_cntr = "00000")  else
+		'1' when  (n_3 < 0) and (d_3 < 0)and (z_cntr = "00000") else
+	  '1' when  (d_3 <= n_3)and (z_cntr = "00001") else
+	  	  '1' when (unsigned(n_3) <= unsigned(shift_left(unsigned(d_3),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0';-- when  signed(n_3) <= shift_left(signed(d_3),1);
 
 
-o_1 <= '1' when (n_1 = 0)and (z_cntr = 0)  else
-		'1' when  (n_1 < 0) and ( (d_1 and register_msb) = 0)and (z_cntr = 0)  else
-		'1' when  (n_1 > 0) and (d_1 < 0)and (z_cntr = 0) else
-	  '1' when n_1 <= d_1 else
-	  '0' when shift_left(signed(d_1),1) <= signed(n_1);
+o_0 <= '1' when (n_0 = 0)and (z_cntr = "00000")  else
+		'1' when  (n_0 < 0) and ( (d_0 and register_msb) = 0)and (z_cntr = "00000")  else
+		'1' when  (n_0 > 0) and (d_0 < 0)and (z_cntr = "00000") else
+	  '1' when n_0 <= d_0 and (z_cntr ="00001") else
+	          	  '1' when (unsigned(d_0) <= unsigned(shift_left(unsigned(n_0),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0';-- when (z_cntr /="00000")and (z_cntr /="00001") and shift_left(signed(d_0),1) <= signed(n_0);
+
+
+o_1 <= '1' when (n_1 = 0)and (z_cntr = "00000")  else
+		'1' when  (n_1 < 0) and ( (d_1 and register_msb) = 0)and (z_cntr = "00000")  else
+		'1' when  (n_1 > 0) and (d_1 < 0)and (z_cntr ="00000") else
+	  '1' when n_1 <= d_1 and (z_cntr ="00001")else
+	          	  '1' when (unsigned(d_1) <= unsigned(shift_left(unsigned(n_1),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0';-- when shift_left(signed(d_1),1) <= signed(n_1);
 
 
 o_2 <= '1' when (n_2 = 0)and (z_cntr = 0)  else
 		'1' when  (n_2 < 0) and ( (d_2 and register_msb) = 0)and (z_cntr = 0)  else
 		'1' when  (n_2 > 0) and (d_2 < 0)and (z_cntr = 0) else
-	  '1' when  n_2 <= d_2 else
-	  '0' when  shift_left(signed(d_2),1) <= signed(n_2);
+	  '1' when  n_2 <= d_2 and (z_cntr ="00001")  else
+	  '1' when (unsigned(d_2) <= unsigned(shift_left(unsigned(n_2),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0';-- when  shift_left(signed(d_2),1) <= signed(n_2);
 
 
 
 o_3 <= '1' when (n_3 = 0)and (z_cntr = 0)  else
 		'1' when  (n_3 < 0) and ( (d_3 and register_msb) = 0)and (z_cntr = 0)  else
 		'1' when  (n_3 > 0) and (d_3 < 0)and (z_cntr = 0) else
-	  '1' when n_3 <= d_3 else
-	  '0' when shift_left(signed(d_3),1) <= signed(n_3);
+	  '1' when n_3 <= d_3 and (z_cntr ="00001")  else
+	  '1' when (unsigned(d_3) <= unsigned(shift_left(unsigned(n_3),1)))and (z_cntr /= "00001") and (z_cntr /= "00000") else
+	  '0';-- when shift_left(signed(d_3),1) <= signed(n_3);
 
 
-
---  A8: if z_cntr = 1 generate
---      z_0 <= (d_0 <= n_0);
---      z_1 <= D_in_in_1 <= n_1;
---      z_2 <= D_in_in_2 <= n_2;
---      z_3 <= D_in_in_3 <= n_3;
-
---      o_0 <= n_0 <= D_in_in_0;
---      o_1 <= n_1 <= D_in_in_1;
---      o_2 <= n_2 <= D_in_in_2;
---      o_3 <= n_3 <= D_in_in_3;
---  end generate A8;
-
---  A9: if z_cntr /= 1 and z_cntr /= 0 generate
---      z_0 <= n_0 <= (d_0 << 1);
---      z_1 <= n_1 <= (d_1 << 1);
---      z_2 <= n_2 <= (d_2 << 1);
---      z_3 <= n_3 <= (d_3 << 1);
-
---      o_0 <= (d_0 << 1) <= n_0;
---      o_1 <= (d_1 << 1) <= n_1;
---      o_2 <= (d_2 << 1) <= n_2;
---      o_3 <= (d_3 << 1) <= n_3;
---  end generate A9;
 
 
 z_a <=  '1' when (z_0 = '1') and (op = Op_NEW) else
